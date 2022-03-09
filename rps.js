4bc15677ab0973a08978e92ea471246b9cd6e299
kieran Sweetman <kikisweetman@gmail.com>
2022-03-09 14:34:04 +0100

    added eventlisteners - score count logic next

--- rps.js
+++ rps.js
@@ -3,8 +3,7 @@
 
 
 
-
-
+// -------------------- RPS LOGIC ---------------------------
 // computers choice
 function computerPlay() {
     var pcMove = Math.floor((Math.random()*3)+1);
@@ -37,7 +36,7 @@ function playerSelection(choice) {
             return 'Scissors';
 
         default:
-            alert("not a valid choice");
+            console.log("not a valid choice");
             return playerSelection(prompt("Rock, Paper or Sciccors?")); 
             
     }   
@@ -46,18 +45,15 @@ function playerSelection(choice) {
  
 
 
-function playRound() {
-    let user_choice = prompt("Rock, Paper or Sciccors?");
+function playRound(user_choice) {
 
     let computerMove = computerPlay();
 
-    let choice = playerSelection(user_choice);
-
-    if (computerMove === choice) {
-        alert("its a draw.");
+    if (computerMove === user_choice) {
+        // console.log("its a draw.");
         return 2;
     } else {
-        let result = winner(choice, computerMove);
+        let result = winner(user_choice, computerMove);
         return result;
     }
 }
@@ -66,28 +62,28 @@ function winner(choice, computerMove) {
     if  (choice === "Rock") {
         //ROCK
         if (computerMove === "Paper") {
-            alert("Scissors, you won");
+            // console.log("Scissors, you won");
             return 1;
         } else {
-            alert("Paper, you lose");
+            // console.log("Paper, you lose");
             return 0;
         }
     } else if (choice === "Paper") {
         //PAPER
         if (computerMove === "Rock") {
-            alert("Rock, you won");
+            // console.log("Rock, you won");
             return 1;
         } else {
-            alert("Scissors, you lose");
+            // console.log("Scissors, you lose");
             return 0;
         }
     } else {
         //SCISSORS
         if (computerMove === "Paper") {
-            alert("Paper, you won");
+            // console.log("Paper, you won");
             return 1;
         } else {
-            alert("Rock, you lose");
+            // console.log("Rock, you lose");
             return 0;
         }
     }
@@ -115,16 +111,52 @@ function winner(choice, computerMove) {
 //     }
 //     if (playerCount > pcCount) {
 //         console.log("You Won! --- Player Score: " + playerCount + " | " + "Pc Score: " + pcCount + " | " + "Draws: " + draw);
-//         return alert("You Won! --- Player Score: " + playerCount + " | " + "Pc Score: " + pcCount + " | " + "Draws: " + draw);
+//         return console.log("You Won! --- Player Score: " + playerCount + " | " + "Pc Score: " + pcCount + " | " + "Draws: " + draw);
         
 
 //     } else if (playerCount < pcCount) {
 //         console.log("You Lost... --- Pc Score: " + pcCount  + " | " + "Player Score: " + playerCount + " | " + "Draws: " + draw);
-//         return alert("You Lost... --- Pc Score: " + pcCount  + " | " + "Player Score: " + playerCount + " | " + "Draws: " + draw);
+//         return console.log("You Lost... --- Pc Score: " + pcCount  + " | " + "Player Score: " + playerCount + " | " + "Draws: " + draw);
         
 //     } else {
 //         console.log("It's a draw... --- Draws: " + draw + " | " + "Player Score: " + playerCount + " | " + "Pc Score: " + pcCount);
-//         return alert("It's a draw... --- Draws: " + draw + " | " + "Player Score: " + playerCount + " | " + "Pc Score: " + pcCount);
+//         return console.log("It's a draw... --- Draws: " + draw + " | " + "Player Score: " + playerCount + " | " + "Pc Score: " + pcCount);
         
 //     }
 // }
+/// ------------------------------end of rps logic--------------------------------------
+
+const choices = document.querySelectorAll(".choice");
+const content = document.querySelector("#content");
+const div = document.createElement('div');
+const p = document.createElement('p');
+
+choices.forEach(input => input.addEventListener('click', () => {
+    let user_choice = (input.getAttribute("name"));
+    let winner = playRound(user_choice);
+    scoreCount(winner);
+    
+}))
+
+// to think about
+function scoreCount(roundWinner) {
+       
+
+    let playerScore = 0;
+    let pcScore = 0;
+    let draw = 0;
+
+    if (playerScore != 5 || pcScore != 5 ) {
+        console.log(playerScore);
+        if (roundWinner == 2) {
+            draw += 1
+        } else if (roundWinner == 1){
+            playerScore += 1
+        } else {
+            pcScore += 1
+        }
+    }
+        
+
+    
+};
